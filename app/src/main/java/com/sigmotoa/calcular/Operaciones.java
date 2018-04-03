737b5e20237f58aec87619a7295284139c115600
GreibelHueso <36461657+GreibelHueso@users.noreply.github.com>
2018-04-03 18:45:30 -0500

    se elaboro operación suma

--- app/src/main/java/com/sigmotoa/calcular/Operaciones.java
+++ app/src/main/java/com/sigmotoa/calcular/Operaciones.java
@@ -15,4 +15,12 @@ public class Operaciones {
     {
         return a+b;
     }
+    
+    public int multiplicar(int a, int b){
+        return a*b;
+    }
+    
+    public double multiplicar(double a, double b){
+        return a*b;
+    }
 }
