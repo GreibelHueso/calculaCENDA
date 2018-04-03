fb2c4d3610dcefd8cfa00d9247b492b95fa8c68f
GreibelHueso <36461657+GreibelHueso@users.noreply.github.com>
2018-04-03 18:46:49 -0500

    se añadió operación en el main

--- app/src/main/java/com/sigmotoa/calcular/Calculadora.java
+++ app/src/main/java/com/sigmotoa/calcular/Calculadora.java
@@ -11,6 +11,8 @@ public class Calculadora {
         Operaciones operar=new Operaciones();
 
         System.out.println(operar.sumar(3.0,5.2));
+        
+        System.out.println(operar.multiplicar(5.2*3.5));
 
     }
 }
